d2f764279d0b0eae2e2b589fbbbb3ce866d38584
Ed Toro <ed@wyncode.co>
2016-11-18 15:54:25 -0500

    fixes for registration form, search results partial, missing migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :registration => "user/registration" }
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, onnly: [:show]
   resources :friendships
